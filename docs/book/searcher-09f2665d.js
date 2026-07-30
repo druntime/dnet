3ace6dd12667e114021fd2af0ea5d6a0f4fa66e6
Daniel Zduniak <dzduniak@gmail.com>
2026-07-30 16:54:11 +0200

    Add more chapters to book.

--- docs/book/searcher-09f2665d.js
+++ docs/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e57e881d.js',
+                path_to_root + 'searchindex-1cf64031.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
